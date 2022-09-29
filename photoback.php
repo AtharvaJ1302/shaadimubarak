5f8c2e376dd635c6818cc495d6b5b3b96afe03e8
Huzaifa-Faizee <96124697+Huzaifa-Faizee@users.noreply.github.com>
2022-09-29 21:55:18 +0530

    changes

--- photoback.php
+++ photoback.php
@@ -11,74 +11,67 @@ window.location.href='photography.php';
 return false;
     }else{return true; }
 }
-if(isset($_POST["venue1"])){
+if(isset($_POST["photo1"])){
     if(check()){
-        $_SESSION["venue"]="90'sBlack";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-}else if(isset($_POST["venue2"])){
-    if(check()){
-        $_SESSION["venue"]="Banga Studio";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue3"])){
-    if(check()){
-        $_SESSION["venue"]="Daas Photography";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-}else if(isset($_POST["venue4"])){
-    if(check()){
-        $_SESSION["venue"]="Light Chamber";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue5"])){
-    if(check()){
-        $_SESSION["venue"]="Parak Studio";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue6"])){
-    if(check()){
-        $_SESSION["venue"]="Photo by the Wedding Ties";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue7"])){
-    if(check()){
-        $_SESSION["venue"]="Suraj Studio";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue8"])){
-    if(check()){
-        $_SESSION["venue"]="Technovision";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue9"])){
-    if(check()){
-        $_SESSION["venue"]="Varun GAvli Studio";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        $_SESSION["photographer"]="90'sBlack";
+        $_SESSION["photographer_price"]=1;
+        header("Location:sound-light.php");
     }
 
-}else if(isset($_POST["venue10"])){
+}else if(isset($_POST["photo2"])){
     if(check()){
-        $_SESSION["venue"]="Vijayeesam Photography";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        $_SESSION["photographer"]="Banga Studio";
+        $_SESSION["photographer_price"]=1;
+        header("Location:sound-light.php");
     }
-    
-}
-    
+}else if(isset($_POST["photo3"])){
+    if(check()){
+        $_SESSION["photographer"]="Daas Photography";
+        $_SESSION["photographer_price"]=1;
+        header("Location:sound-light.php");
+    }
+}else if(isset($_POST["photo4"])){
+    if(check()){
+        $_SESSION["photographer"]="Light Chamber";
+        $_SESSION["photographer_price"]=1;
+        header("Location:sound-light.php");
+    }
+}else if(isset($_POST["photo5"])){
+    if(check()){
+        $_SESSION["photographer"]="Parak Studio";
+        $_SESSION["photographer_price"]=1;
+        header("Location:sound-light.php");
+    }
+}else if(isset($_POST["photo6"])){
+    if(check()){
+        $_SESSION["photographer"]="Photo by the Wedding Ties";
+        $_SESSION["photographer_price"]=1;
+        header("Location:sound-light.php");
+    }
+}else if(isset($_POST["photo7"])){
+    if(check()){
+        $_SESSION["photographer"]="Suraj Studio";
+        $_SESSION["photographer_price"]=1;
+        header("Location:sound-light.php");
+    }
+}else if(isset($_POST["photo8"])){
+    if(check()){
+        $_SESSION["photographer"]="Technovision";
+        $_SESSION["photographer_price"]=1;
+        header("Location:sound-light.php");
+    }
+}else if(isset($_POST["photo9"])){
+    if(check()){
+        $_SESSION["photographer"]="Varun Gavli Studio";
+        $_SESSION["photographer_price"]=1;
+        header("Location:sound-light.php");
+    }
+}else if(isset($_POST["photo10"])){
+    if(check()){
+        $_SESSION["photographer"]="Vijayeesam Photography";
+        $_SESSION["photographer_price"]=1;
+        header("Location:sound-light.php");
+    }
+} 
+
 ?>
--- soundandlights.php
+++ soundandlights.php
@@ -1,84 +1,85 @@
 <?php
 session_start();
-function check()
 {
     if(empty($_SESSION))
     {
         echo "<script>
 alert('Signin To Start Booking!');
-window.location.href='';
+window.location.href='sound-light.php';
 </script>";
 return false;
     }else{return true; }
 }
-if(isset($_POST["venue1"])){
+if(isset($_SESSION["light1"])){
     if(check()){
-        $_SESSION["venue"]="";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-}else if(isset($_POST["venue2"])){
-    if(check()){
-        $_SESSION["venue"]="";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue3"])){
-    if(check()){
-        $_SESSION["venue"]="";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-}else if(isset($_POST["venue4"])){
-    if(check()){
-        $_SESSION["venue"]="";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue5"])){
-    if(check()){
-        $_SESSION["venue"]="";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue6"])){
-    if(check()){
-        $_SESSION["venue"]="";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue7"])){
-    if(check()){
-        $_SESSION["venue"]="";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue8"])){
-    if(check()){
-        $_SESSION["venue"]="";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
-    }
-    
-}else if(isset($_POST["venue9"])){
-    if(check()){
-        $_SESSION["venue"]="";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        $_SESSION["light"]="Absolut Lime";
+        $_SESSION["light_price"]=1;
+        header("Location:dummyreceipt.html");
     }
 
-}else if(isset($_POST["venue10"])){
+}else if(isset($_SESSION["light2"])){
     if(check()){
-        $_SESSION["venue"]="";
-        $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        $_SESSION["light"]="D&S Djs";
+        $_SESSION["light_price"]=1;
+        header("Location:dummyreceipt.html");
     }
-    
+
+}else if(isset($_SESSION["light3"])){
+    if(check()){
+        $_SESSION["light"]="Dj Ganesh";
+        $_SESSION["light_price"]=1;
+        header("Location:dummyreceipt.html");
+    }
+
+}else if(isset($_SESSION["light4"])){
+    if(check()){
+        $_SESSION["light"]="Dj Santosh";
+        $_SESSION["light_price"]=1;
+        header("Location:dummyreceipt.html");
+    }
+
+}else if(isset($_SESSION["light5"])){
+    if(check()){
+        $_SESSION["light"]="Dj Vibes";
+        $_SESSION["light_price"]=1;
+        header("Location:dummyreceipt.html");
+    }
+
+}else if(isset($_SESSION["light6"])){
+    if(check()){
+        $_SESSION["light"]="Dj Wale Babu";
+        $_SESSION["light_price"]=1;
+        header("Location:dummyreceipt.html");
+    }
+
+}else if(isset($_SESSION["light7"])){
+    if(check()){
+        $_SESSION["light"]="Elite Dj Planners";
+        $_SESSION["light_price"]=1;
+        header("Location:dummyreceipt.html");
+    }
+
+}else if(isset($_SESSION["light8"])){
+    if(check()){
+        $_SESSION["light"]="Miami Djs";
+        $_SESSION["light_price"]=1;
+        header("Location:dummyreceipt.html");
+    }
+
+}else if(isset($_SESSION["light9"])){
+    if(check()){
+        $_SESSION["light"]="Royal Djs";
+        $_SESSION["light_price"]=1;
+        header("Location:dummyreceipt.html");
+    }
+
+}else if(isset($_SESSION["light10"])){
+    if(check()){
+        $_SESSION["light"]="RP Dj Wale";
+        $_SESSION["light_price"]=1;
+        header("Location:dummyreceipt.html");
+    }
+
 }
-    
+ 
 ?>
