7b17267413f7a603ff4c31c767b468d7f5685f4e
Huzaifa-Faizee <96124697+Huzaifa-Faizee@users.noreply.github.com>
2022-09-30 17:36:07 +0530

    integration

--- venuedelhi.php
+++ venuedelhi.php
@@ -15,68 +15,68 @@ if(isset($_POST["venue1"])){
     if(check()){
         $_SESSION["venue"]="Calista Resort";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
 }else if(isset($_POST["venue2"])){
     if(check()){
         $_SESSION["venue"]="Veda Farms";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue3"])){
     if(check()){
         $_SESSION["venue"]="Amrai Farms";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
 }else if(isset($_POST["venue4"])){
     if(check()){
         $_SESSION["venue"]="Solitaire Garden and Banquet";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue5"])){
     if(check()){
         $_SESSION["venue"]="Araliayas Resort";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue6"])){
     if(check()){
         $_SESSION["venue"]="Ramada Resort";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue7"])){
     if(check()){
         $_SESSION["venue"]="Yadu Greens";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue8"])){
     if(check()){
         $_SESSION["venue"]="Mohan Vilaas";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue9"])){
     if(check()){
         $_SESSION["venue"]="Elden Ring Motel And Resorts";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
 
 }else if(isset($_POST["venue10"])){
     if(check()){
         $_SESSION["venue"]="Hilltop Greens";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }
