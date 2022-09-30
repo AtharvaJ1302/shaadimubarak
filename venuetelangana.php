97397ba4c84fabde3b2f49576acae90b40db482e
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2022-09-30 17:38:36 +0530

    changes

--- venuetelangana.php
+++ venuetelangana.php
@@ -15,68 +15,68 @@ if(isset($_POST["venue1"])){
     if(check()){
         $_SESSION["venue"]="Taj Falaknuma Palace";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
 }else if(isset($_POST["venue2"])){
     if(check()){
         $_SESSION["venue"]="Chowmahalla Palace";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue3"])){
     if(check()){
         $_SESSION["venue"]="Fort Grand";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
 }else if(isset($_POST["venue4"])){
     if(check()){
         $_SESSION["venue"]="Taj Krishna";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue5"])){
     if(check()){
         $_SESSION["venue"]="Courtyard by Marriot";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue6"])){
     if(check()){
         $_SESSION["venue"]="The Golkonda Resorts";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue7"])){
     if(check()){
         $_SESSION["venue"]="Aalankrita Resort";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue8"])){
     if(check()){
         $_SESSION["venue"]="Sri Venkateswara Garden";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }else if(isset($_POST["venue9"])){
     if(check()){
         $_SESSION["venue"]="Imperial Gardens";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
 
 }else if(isset($_POST["venue10"])){
     if(check()){
         $_SESSION["venue"]="Summer Green Resort Palm Groves";
         $_SESSION["venue_price"]=1;
-        header("Location:dummyfood.html");
+        header("Location:food.php");
     }
     
 }
