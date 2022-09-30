1951da9a58f287c1a9ca14c59d3979997ab1d427
Huzaifa-Faizee <96124697+Huzaifa-Faizee@users.noreply.github.com>
2022-09-30 16:44:37 +0530

    Update foodback.php

--- foodback.php
+++ foodback.php
@@ -1,3 +1,25 @@
 <?php
-//food backend will be here
+sesssion_start();
+if(isset($_POST["submit"])){
+    $food_list=$_POST["food"];
+    //convert each value to integer so we can fetch from database
+    $food=array();
+    for($i=0;$i<count($food_list);$i++)
+    {
+        $food[$i]=(int)$food_list[$i]
+    }
+    //pass values to session variable
+    $_SESSION["food"]=$food;
+    
+    //check if valus passed to session variable 
+    // foreach($_SESSION["food"] as $val )
+    // {
+    //     echo $val."<br>";
+    // }
+
+    //head to next page
+
+    //header("Location:photography.php");
+
+}
 ?>
