f4fb9c4e57b36a92574195b649558da2e42b2f5b
Huzaifa-Faizee <96124697+Huzaifa-Faizee@users.noreply.github.com>
2022-11-15 19:41:03 +0530

    Update invoice.php

--- invoice.php
+++ invoice.php
@@ -9,7 +9,7 @@ $pdf->Image('logo.jpeg',10,10,30,30);
 $pdf->SETXY(100,23);
 $pdf->Cell(50,5,'INVOICE',0,0);
 $receipt=$_SESSION["receipt"];
-echo $receipt;
+//echo $receipt;
 $sql="SELECT * FROM transactions WHERE receipt_id='$receipt'";
 $result=mysqli_query($conn,$sql);
 if(!$result) {
@@ -23,8 +23,8 @@ $pdf->Row($mc_array,0);
 $grandtotal=0;
 while($row=mysqli_fetch_array($result, MYSQLI_ASSOC) ) {
  
-          $cusname=$row["name"];
-          $cusemail=$row["email"];
+          $cusname=$_SESSION["myname"];
+          $cusemail=$_SESSION["email"];
           $cusinvoice=$row["receipt_id"];
           $venue_name=$row["venue_name"];
           $venue_price=$row["venue_price"];
@@ -33,7 +33,15 @@ while($row=mysqli_fetch_array($result, MYSQLI_ASSOC) ) {
           $photo_name=$row["photo_name"];
           $photo_price=$row["photo_price"];
           $food_total=$row["food_total"];
-          $grandtotal=$grandtotal+$venue_price+$light_price+$photo_price+$food_total;
+          $mehendi_name=$row["mehendi_name"];
+            $mehendi_price=$row["mehendi_price"];
+            $inc_name=$row["inv_card_name"];
+            $inc_price=$row["inv_card_price"];
+            $tran_name=$row["tran_name"];
+            $tran_price=$row["tran_price"];
+            $food=$row["food_id"];
+            $food_arr=explode("|",$food);
+         $grandtotal=$grandtotal+$venue_price+$light_price+$photo_price+$food_total+$mehendi_price+$inc_price+$tran_price;
 
 $pdf->SetXY(10,40);
 $pdf->Cell(60,5,'Invoice Number: '.$cusinvoice,0,0); // receipt number= userid + timestamp
@@ -43,24 +51,58 @@ $pdf->SetXY(10,50);
 $pdf->Cell(120,5,'Customer Name: '.$cusname,0,0);
 $pdf->SetXY(10,60);
 $pdf->Cell(120,5,'Customer Email-id: '.$cusemail,0,0); 
+$n=77;
 $mc_array=array($venue_name,$venue_price,$venue_price);
-$pdf->SetXY(10,77);
+$pdf->SetXY(10,$n);
 $pdf->Row($mc_array,0);
 
+$n=$n+7;
 $mc_array=array($light_name,$light_price,$light_price);
-$pdf->SetXY(10,84);
+$pdf->SetXY(10,$n);
 $pdf->Row($mc_array,0);
 
+
+$n=$n+7;
 $mc_array=array($photo_name,$photo_price,$photo_price);
-$pdf->SetXY(10,91);
+$pdf->SetXY(10,$n);
 $pdf->Row($mc_array,0);
 
-$mc_array=array("Food",$food_total,$food_total);
-$pdf->SetXY(10,98);
+foreach($food_arr as $f){
+    if($f!=null){
+        $id=(int)$f;
+        $sql="SELECT * FROM food WHERE food_id='$id'";
+        $result=mysqli_query($conn,$sql);
+        $k=mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $n=$n+7;
+        $food_name=$k["food_name"];
+        $price=$k["food_price"];
+        $mc_array=array($food_name,$price,$price);
+        $pdf->SetXY(10,$n);
+        $pdf->Row($mc_array,0);
+    }
+    
+}
+
+
+
+$n=$n+7;
+$mc_array=array($mehendi_name,$mehendi_price,$mehendi_price);
+$pdf->SetXY(10,$n);
 $pdf->Row($mc_array,0);
-} 
+
+$n=$n+7;
+$mc_array=array($tran_name,$tran_price,$tran_price);
+$pdf->SetXY(10,$n);
+$pdf->Row($mc_array,0);
+
+$n=$n+7;
+$mc_array=array($inc_name,$inc_price,$inc_price);
+$pdf->SetXY(10,$n);
+$pdf->Row($mc_array,0);
+}
+$n=$n+7; 
 $mc_array=array("","Grand Total",$grandtotal);
-$pdf->SetXY(10,105);
+$pdf->SetXY(10,$n);
 $pdf->Row($mc_array,0);
 
 $filename="invoices/".$cusinvoice;
